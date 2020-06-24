e6a98e2cdf6508d07a0a41a1008a10609e10093b
John Doan <johndoan42@gmail.com>
2020-06-24 16:51:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,8 +2,8 @@
 require 'yaml'
 require 'pry'
 yaml_file = "lib/emoticons.yml"
-def load_library(yaml_file)
-  emoticon_library = YAML.load_file(yaml_file)
+def load_library
+  emoticon_library = YAML.load_file('lib/emoticons.yml')
   emoticon_library
 end
 
